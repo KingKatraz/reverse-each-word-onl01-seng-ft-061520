37bd3b50321073b975358ecc81cd03c16bba9a5d
Hal H Nader <halnader89@gmail.com>
2020-06-26 23:47:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 
-def reverse_each_word(sentence1)
-  sentence1.each do   |sentence1|
+def reverse_each_word(sentence)
+  sentence.each do   |sentence|
     "Hello there, and how are you?"
   end
 end
