46ec6fd5433303aa665925e7dcd67510f81f52fa
Hal H Nader <halnader89@gmail.com>
2020-06-27 00:12:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-  sentence.split.collect do{|word| word.reverse}.join(" ")
+  sentence.split.collect do 
+    {|word| word.reverse}.join(" ")
 end
 
 sentence.split.collect {|word| word.reverse}.join(" ")
